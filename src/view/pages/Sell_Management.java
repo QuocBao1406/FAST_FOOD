d65c99b854caaf836fb057c3834f707f873b0d7c
QuocBao1406 <baonhq.se@gmail.com>
2024-12-17 14:00:23 +0700

    xong phần food

--- src/view/pages/Sell_Management.java
+++ src/view/pages/Sell_Management.java
@@ -6,9 +6,13 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import java.awt.GridLayout;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
 
 public class Sell_Management extends JPanel{
 	public Sell_Management() {
+		setBorder(new EmptyBorder(5, 5, 5, 5));
 		setBackground(new Color(255, 242, 189));
 		setSize(1250, 820);
 		setLayout(null);
@@ -19,7 +23,7 @@ public class Sell_Management extends JPanel{
 		add(lb_title);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 114, 862, 666);
+		scrollPane.setBounds(10, 100, 862, 680);
 		add(scrollPane);
 	}
 }
